cab0124a865d0cf58fb6fc59fa38903e4d5ec4f3
Marcos Gómez Vilches <marcos@gesdinet.com>
2016-01-29 08:46:34 +0100

    Moved SimpleAuthenticatorInterface

--- Security/Authenticator/RefreshTokenAuthenticator.php
+++ Security/Authenticator/RefreshTokenAuthenticator.php
@@ -12,7 +12,7 @@
 namespace Gesdinet\JWTRefreshTokenBundle\Security\Authenticator;
 
 use Gesdinet\JWTRefreshTokenBundle\Request\RequestRefreshToken;
-use Symfony\Component\Security\Core\Authentication\SimplePreAuthenticatorInterface;
+use Symfony\Component\Security\Http\Authentication\SimplePreAuthenticatorInterface;
 use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
 use Symfony\Component\Security\Core\Exception\AuthenticationException;
 use Symfony\Component\Security\Core\Authentication\Token\PreAuthenticatedToken;
