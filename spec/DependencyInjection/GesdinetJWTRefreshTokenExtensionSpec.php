79337d01c61edb871a19f7063094ec1a4b31c339
Toby Griffiths <toby@cubicmushroom.co.uk>
2017-12-12 12:11:08 +0000

    Added expected setParameter calls
    … partly becuse I had to set the expected response for fileExists() due
    to PHP7's strict return types
    
    Ignoring service definition setting in test as relates to parameters

--- spec/DependencyInjection/GesdinetJWTRefreshTokenExtensionSpec.php
+++ spec/DependencyInjection/GesdinetJWTRefreshTokenExtensionSpec.php
@@ -2,7 +2,9 @@
 
 namespace spec\Gesdinet\JWTRefreshTokenBundle\DependencyInjection;
 
+use Gesdinet\JWTRefreshTokenBundle\Entity\RefreshToken;
 use PhpSpec\ObjectBehavior;
+use Prophecy\Argument;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 
 class GesdinetJWTRefreshTokenExtensionSpec extends ObjectBehavior
@@ -12,9 +14,30 @@ class GesdinetJWTRefreshTokenExtensionSpec extends ObjectBehavior
         $this->shouldHaveType('Gesdinet\JWTRefreshTokenBundle\DependencyInjection\GesdinetJWTRefreshTokenExtension');
     }
 
+
     public function it_should_set_parameters_correctly(ContainerBuilder $container)
     {
-        $configs = array();
+        $container->fileExists(dirname(dirname(__DIR__)).'/DependencyInjection/../Resources/config/services.yml')
+                  ->willReturn(true);
+
+        // setParameter calls with default values
+        $container->setParameter('gesdinet_jwt_refresh_token.ttl', 2592000)
+                  ->shouldBeCalled();
+        $container->setParameter('gesdinet_jwt_refresh_token.ttl_update', false)
+                  ->shouldBeCalled();
+        $container->setParameter('gesdinet_jwt_refresh_token.security.firewall', 'api')
+                  ->shouldBeCalled();
+        $container->setParameter('gesdinet_jwt_refresh_token.user_provider', null)
+                  ->shouldBeCalled();
+        $container->setParameter('gesdinet.jwtrefreshtoken.refresh_token.class', RefreshToken::class)
+                  ->shouldBeCalled();
+        $container->setParameter('gesdinet.jwtrefreshtoken.entity_manager.id', 'doctrine.orm.entity_manager')
+                  ->shouldBeCalled();
+
+        $container->setDefinition(Argument::cetera())
+                  ->willReturn();
+
+        $configs = [];
         $this->load($configs, $container);
     }
 }
