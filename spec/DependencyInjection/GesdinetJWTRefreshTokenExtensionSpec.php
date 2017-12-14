82985cec8b475f045eecc9f65c9dedf204c02d78
Toby Griffiths <toby@cubicmushroom.co.uk>
2017-12-14 10:08:06 +0000

    Attempt to fix error with tests on Travis
    This it might be due to setting up stubs in let() when not using beConstrictedWith()

--- spec/DependencyInjection/GesdinetJWTRefreshTokenExtensionSpec.php
+++ spec/DependencyInjection/GesdinetJWTRefreshTokenExtensionSpec.php
@@ -9,14 +9,6 @@ use Symfony\Component\DependencyInjection\ContainerBuilder;
 
 class GesdinetJWTRefreshTokenExtensionSpec extends ObjectBehavior
 {
-    public function let(ContainerBuilder $container)
-    {
-        $container->fileExists(dirname(dirname(__DIR__)).'/DependencyInjection/../Resources/config/services.yml')
-                  ->willReturn(true);
-        $container->has('gesdinet.jwtrefreshtoken.name_generator.underscore')
-                  ->willReturn(true);
-    }
-
     public function it_is_initializable()
     {
         $this->shouldHaveType('Gesdinet\JWTRefreshTokenBundle\DependencyInjection\GesdinetJWTRefreshTokenExtension');
@@ -24,6 +16,11 @@ class GesdinetJWTRefreshTokenExtensionSpec extends ObjectBehavior
 
     public function it_should_set_parameters_correctly(ContainerBuilder $container)
     {
+        $container->fileExists(dirname(dirname(__DIR__)).'/DependencyInjection/../Resources/config/services.yml')
+                  ->willReturn(true);
+        $container->has('gesdinet.jwtrefreshtoken.name_generator.underscore')
+                  ->willReturn(true);
+
         // setParameter calls with default values
         $container->setParameter('gesdinet_jwt_refresh_token.ttl', 2592000)
                   ->shouldBeCalled();
@@ -52,6 +49,11 @@ class GesdinetJWTRefreshTokenExtensionSpec extends ObjectBehavior
 
     public function it_should_configure_the_default_naming_generator(ContainerBuilder $container)
     {
+        $container->fileExists(dirname(dirname(__DIR__)).'/DependencyInjection/../Resources/config/services.yml')
+                  ->willReturn(true);
+        $container->has('gesdinet.jwtrefreshtoken.name_generator.underscore')
+                  ->willReturn(true);
+
         $container->setAlias(
             'gesdinet.jwtrefreshtoken.name_generator.default',
             Argument::exact(new Alias('gesdinet.jwtrefreshtoken.name_generator.underscore'))
@@ -71,6 +73,9 @@ class GesdinetJWTRefreshTokenExtensionSpec extends ObjectBehavior
     public function it_should_throw_an_exception_if_specifying_a_non_existent_name_generator_service(
         ContainerBuilder $container
     ) {
+        $container->fileExists(dirname(dirname(__DIR__)).'/DependencyInjection/../Resources/config/services.yml')
+                  ->willReturn(true);
+
         $container->has('some.service.name')
                   ->willReturn(false);
 
@@ -89,6 +94,12 @@ class GesdinetJWTRefreshTokenExtensionSpec extends ObjectBehavior
 
     public function it_should_configure_a_custom_name_generator(ContainerBuilder $container)
     {
+        $container->fileExists(dirname(dirname(__DIR__)).'/DependencyInjection/../Resources/config/services.yml')
+                  ->willReturn(true);
+        
+        $container->has('gesdinet.jwtrefreshtoken.name_generator.underscore')
+                  ->willReturn(true);
+
         // Expectations
         $container->setAlias('gesdinet.jwtrefreshtoken.name_generator.default', 'some.service.name')
                   ->shouldBeCalled();
