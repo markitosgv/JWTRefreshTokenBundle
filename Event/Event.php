ad475378b278719bebe4062c50b6135c5bcdd4e7
Albert Casademont <albertcasademont@gmail.com>
2019-07-03 08:42:07 +0200

    Remove Symfony 4.3 deprecations (#156)

---
+++ Event/Event.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Gesdinet\JWTRefreshTokenBundle\Event;
+
+use Symfony\Component\EventDispatcher\Event as BaseEvent;
+use Symfony\Component\EventDispatcher\EventDispatcher;
+use Symfony\Contracts\EventDispatcher\Event as ContractsBaseEvent;
+use Symfony\Contracts\EventDispatcher\EventDispatcherInterface;
+
+if (is_subclass_of(EventDispatcher::class, EventDispatcherInterface::class)) {
+    class Event extends ContractsBaseEvent
+    {
+    }
+} else {
+    class Event extends BaseEvent
+    {
+    }
+}
