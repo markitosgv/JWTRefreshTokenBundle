b74e7a1e2bab4421eb5f294c10216e8dfc879719
Ihor Melnichenko <igor@creawill.com>
2019-08-30 12:46:01 +0300

    Refactoring a crutch for backward compatibility

---
+++ Document/DocumentRepository.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Gesdinet\JWTRefreshTokenBundle\Document;
+
+use Doctrine\ODM\MongoDB\DocumentRepository as OldDocumentRepository;
+use Doctrine\ODM\MongoDB\Repository\DocumentRepository as ForwardDocumentRepository;
+
+// @todo: remove this condition and extend directly from `ForwardDocumentRepository`
+// when support for "doctrine/mongodb-odm <2.0" is removed.
+if (class_exists(ForwardDocumentRepository::class, false)) {
+    class BaseRepository extends ForwardDocumentRepository
+    {
+    }
+} else {
+    class BaseRepository extends DocumentRepository
+    {
+    }
+}
+
+/**
+ * Class DocumentRepository
+ * @package Gesdinet\JWTRefreshTokenBundle\Document
+ */
+class DocumentRepository extends BaseRepository
+{
+
+}
