cba48da8da3af853a5dc876c991923e4531c85c3
Sherin <sherin@blueberry.nl>
2020-08-06 11:32:47 +0200

    Create RefreshTokenCreatedEvent

---
+++ Event/RefreshTokenCreatedEvent.php
@@ -0,0 +1,28 @@
+<?php
+
+
+namespace Gesdinet\JWTRefreshTokenBundle\Event;
+
+
+use Gesdinet\JWTRefreshTokenBundle\Model\RefreshTokenInterface;
+
+class RefreshTokenCreatedEvent extends Event
+{
+    /**
+     * @var RefreshTokenInterface
+     */
+    private $refreshToken;
+
+    public function __construct(RefreshTokenInterface $refreshToken)
+    {
+        $this->refreshToken = $refreshToken;
+    }
+
+    /**
+     * @return RefreshTokenInterface
+     */
+    public function getRefreshToken()
+    {
+        return $this->refreshToken;
+    }
+}
