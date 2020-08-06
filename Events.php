ea76950779f971705eb8ae2aa781c374bba7c2a8
Sherin <sherin@blueberry.nl>
2020-08-06 11:32:30 +0200

    Created Events.php that contains constants for event ids

---
+++ Events.php
@@ -0,0 +1,29 @@
+<?php
+
+
+namespace Gesdinet\JWTRefreshTokenBundle;
+
+/**
+ * Events.
+ *
+ * @author Sherin Bloemendaal <sherin@dactec.nl>
+ */
+final class Events
+{
+    /**
+     * Dispatched after the token refresh.
+     * Hook into this event to listen to successful token refreshes.
+     *
+     * @Event("Gesdinet\JWTRefreshTokenBundle\Event\RefreshEvent")
+     */
+    const ON_REFRESH_TOKEN = 'gesdinet.refresh_token';
+
+    /**
+     * Dispatched before the refresh token is persisted into the database.
+     * Hook into this event to fill the extra fields when using a custom entity.
+     *
+     * @Event("Gesdinet\JWTRefreshTokenBundle\Event\RefreshTokenCreatedEvent")
+     */
+    const REFRESH_TOKEN_CREATED = 'gesdinet.refresh_token_created';
+}
+
