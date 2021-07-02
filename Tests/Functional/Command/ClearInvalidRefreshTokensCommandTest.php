7393d0dc55d846ce35ef6ea5e86cf05e212d29ba
Michael Babker <michael.babker@gmail.com>
2021-07-01 19:37:49 -0500

    Use PHPUnit for command and DI tests

---
+++ Tests/Functional/Command/ClearInvalidRefreshTokensCommandTest.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace Gesdinet\JWTRefreshTokenBundle\Tests\Functional\Command;
+
+use Gesdinet\JWTRefreshTokenBundle\Command\ClearInvalidRefreshTokensCommand;
+use Gesdinet\JWTRefreshTokenBundle\Model\RefreshTokenInterface;
+use Gesdinet\JWTRefreshTokenBundle\Model\RefreshTokenManagerInterface;
+use PHPUnit\Framework\MockObject\MockObject;
+use PHPUnit\Framework\TestCase;
+use Symfony\Component\Console\Tester\CommandTester;
+
+final class ClearInvalidRefreshTokensCommandTest extends TestCase
+{
+    public function test_clears_tokens_without_timestamp(): void
+    {
+        /** @var MockObject&RefreshTokenInterface $refreshToken */
+        $refreshToken = $this->createMock(RefreshTokenInterface::class);
+        $refreshToken->expects($this->once())
+            ->method('getRefreshToken')
+            ->willReturn('refresh-token');
+
+        /** @var MockObject&RefreshTokenManagerInterface $refreshTokenManager */
+        $refreshTokenManager = $this->createMock(RefreshTokenManagerInterface::class);
+        $refreshTokenManager->expects($this->once())
+            ->method('revokeAllInvalid')
+            ->with($this->isInstanceOf(\DateTimeInterface::class))
+            ->willReturn([$refreshToken]);
+
+        $command = new ClearInvalidRefreshTokensCommand($refreshTokenManager);
+
+        $commandTester = new CommandTester($command);
+        $commandTester->execute([]);
+
+        $this->assertSame(0, $commandTester->getStatusCode());
+        $this->assertStringContainsString('Revoke refresh-token', $commandTester->getDisplay());
+    }
+
+    public function test_clears_tokens_with_timestamp(): void
+    {
+        /** @var MockObject&RefreshTokenInterface $refreshToken */
+        $refreshToken = $this->createMock(RefreshTokenInterface::class);
+        $refreshToken->expects($this->once())
+            ->method('getRefreshToken')
+            ->willReturn('refresh-token');
+
+        /** @var MockObject&RefreshTokenManagerInterface $refreshTokenManager */
+        $refreshTokenManager = $this->createMock(RefreshTokenManagerInterface::class);
+        $refreshTokenManager->expects($this->once())
+            ->method('revokeAllInvalid')
+            ->with($this->isInstanceOf(\DateTimeInterface::class))
+            ->willReturn([$refreshToken]);
+
+        $command = new ClearInvalidRefreshTokensCommand($refreshTokenManager);
+
+        $commandTester = new CommandTester($command);
+        $commandTester->execute(['datetime' => '2021-01-01']);
+
+        $this->assertSame(0, $commandTester->getStatusCode());
+        $this->assertStringContainsString('Revoke refresh-token', $commandTester->getDisplay());
+    }
+}
---
+++ Tests/Functional/DependencyInjection/ConfigurationTest.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Gesdinet\JWTRefreshTokenBundle\Tests\Functional\DependencyInjection;
+
+use Gesdinet\JWTRefreshTokenBundle\DependencyInjection\Configuration;
+use Gesdinet\JWTRefreshTokenBundle\Document\RefreshToken;
+use Matthias\SymfonyConfigTest\PhpUnit\ConfigurationTestCaseTrait;
+use PHPUnit\Framework\TestCase;
+use Symfony\Component\Config\Definition\ConfigurationInterface;
+
+final class ConfigurationTest extends TestCase
+{
+    use ConfigurationTestCaseTrait;
+
+    protected function getConfiguration(): ConfigurationInterface
+    {
+        return new Configuration();
+    }
+
+    public function test_default_configuration_is_valid(): void
+    {
+        $this->assertConfigurationIsValid([]);
+    }
+
+    public function test_custom_configuration_is_valid(): void
+    {
+        $this->assertConfigurationIsValid([
+            [
+                'ttl' => 123,
+                'ttl_update' => true,
+                'firewall' => 'main',
+                'user_provider' => 'my.user_provider',
+                'user_identity_field' => 'email',
+                'manager_type' => 'mongodb',
+                'refresh_token_class' => RefreshToken::class,
+                'object_manager' => 'doctrine_mongodb.odm.document_manager',
+                'user_checker' => 'my.user_checker',
+                'single_use' => true,
+                'token_parameter_name' => 'the_token',
+                'doctrine_mappings' => false,
+            ],
+        ]);
+    }
+}
