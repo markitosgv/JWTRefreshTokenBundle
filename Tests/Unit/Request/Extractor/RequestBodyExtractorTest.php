47c9b86a18cdaf1050fcb3b7921e230b7301dde7
Jon Cram <webignition@gmail.com>
2022-01-09 15:35:56 +0000

    Translate spec/Request/Extractor/RequestBodyExtractorSpec.php

---
+++ Tests/Unit/Request/Extractor/RequestBodyExtractorTest.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace Gesdinet\JWTRefreshTokenBundle\Tests\Unit\Request\Extractor;
+
+use Gesdinet\JWTRefreshTokenBundle\Request\Extractor\ExtractorInterface;
+use Gesdinet\JWTRefreshTokenBundle\Request\Extractor\RequestBodyExtractor;
+use PHPUnit\Framework\MockObject\MockObject;
+use PHPUnit\Framework\TestCase;
+use Symfony\Component\HttpFoundation\Request;
+
+class RequestBodyExtractorTest extends TestCase
+{
+    private const PARAMETER_NAME = 'refresh_token';
+    private RequestBodyExtractor $requestBodyExtractor;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->requestBodyExtractor = new RequestBodyExtractor();
+    }
+
+    public function testIsAnExtractor(): void
+    {
+        $this->assertInstanceOf(ExtractorInterface::class, $this->requestBodyExtractor);
+    }
+
+    public function testGetsTheTokenFromTheRequestBody(): void
+    {
+        $token = 'my-refresh-token';
+        $request = $this->createMockRequest('json', [self::PARAMETER_NAME => $token]);
+
+        $this->assertSame(
+            $token,
+            $this->requestBodyExtractor->getRefreshToken($request, self::PARAMETER_NAME)
+        );
+    }
+
+    public function testReturnsNullIfTheParameterDoesNotExistInTheRequestBody(): void
+    {
+        $request = $this->createMockRequest('json', []);
+
+        $this->assertNull($this->requestBodyExtractor->getRefreshToken($request, self::PARAMETER_NAME));
+    }
+
+    public function testReturnsNullIfTheRequestIsNotAJsonType(): void
+    {
+        $request = $this->createMockRequest( null);
+
+        $this->assertNull($this->requestBodyExtractor->getRefreshToken($request, self::PARAMETER_NAME));
+    }
+
+    /**
+     * @return Request|MockObject
+     */
+    private function createMockRequest(?string $contentType, ?array $jsonBodyData = null): MockObject
+    {
+        /** @var Request|MockObject $request */
+        $request = $this->createMock(Request::class);
+
+        $request
+            ->expects($this->once())
+            ->method('getContentType')
+            ->willReturn($contentType);
+
+        if (is_array($jsonBodyData)) {
+            $request
+                ->expects($this->once())
+                ->method('getContent')
+                ->willReturn(json_encode($jsonBodyData));
+        }
+
+        return $request;
+    }
+}
