e64012c8ae83a54d5ffd1233a04cdbc8dde6740b
Michael Babker <michael.babker@gmail.com>
2022-04-20 22:01:08 -0500

    Remove unused code

--- Resources/config/services.php
+++ Resources/config/services.php
@@ -19,7 +19,6 @@ use Gesdinet\JWTRefreshTokenBundle\Security\Http\Authentication\AuthenticationFa
 use Gesdinet\JWTRefreshTokenBundle\Security\Http\Authentication\AuthenticationSuccessHandler;
 use Gesdinet\JWTRefreshTokenBundle\Security\Http\Authenticator\RefreshTokenAuthenticator;
 use Lexik\Bundle\JWTAuthenticationBundle\Events;
-use Symfony\Component\Security\Http\Event\LogoutEvent;
 
 return static function (ContainerConfigurator $container) {
     $services = $container->services();
